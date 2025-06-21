83b5174a356193b4755ee3a67297b7ce22dec9f4
NandiniP-1234 <nandinipatel345@gmail.com>
2025-06-21 18:34:01 +0530

    Add basic clock

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.util.Date;
+import javax.swing.*;
+import java.awt.*;         
+import java.awt.event.*;
+import java.text.SimpleDateFormat;
+public class Main {
+    public static void main(String[] args){
+       MyWindow m1 = new MyWindow();
+    }
+}
+class MyWindow extends JFrame{
+
+    private JLabel heading;
+    private JLabel clockLabel;
+    private Font font = new Font("" , Font.BOLD , 35);
+
+    MyWindow(){
+        setTitle("My Clock");
+        setSize(400 , 400);
+        setLocation(300 , 50);
+        this.createGui();
+        this.startClock();
+        setVisible(true);
+    }
+
+    public void createGui(){
+        heading = new JLabel("My Clock");
+        clockLabel = new JLabel("clock");
+        heading.setFont(font);
+        clockLabel.setFont(font);
+        this.setLayout(new GridLayout(2 , 1));
+        this.add(heading);
+        this.add(clockLabel);
+
+        
+    }
+
+    public void startClock(){
+        
+        Thread t = new Thread(){
+            public void run(){
+            try{
+                while(true){
+                   Date d = new Date();
+                   SimpleDateFormat sdf = new SimpleDateFormat("hh : mm : ss a");
+                   String dateTime = sdf.format(d);
+                   SwingUtilities.invokeLater(() -> clockLabel.setText(dateTime));
+                   Thread.currentThread().sleep(1000);
+                }
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    };
+        t.start();
+    }
+}
